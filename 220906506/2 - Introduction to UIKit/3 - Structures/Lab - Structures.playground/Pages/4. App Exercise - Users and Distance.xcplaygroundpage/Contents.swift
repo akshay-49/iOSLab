4961e7d5c599355f4570408eafedd59cfc4a278e
akshay-49 <akshay.49@outlook.com>
2025-02-04 15:02:29 +0530

    4th Feb commit midway structures

--- 220906506/2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/4. App Exercise - Users and Distance.xcplaygroundpage/Contents.swift
+++ 220906506/2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/4. App Exercise - Users and Distance.xcplaygroundpage/Contents.swift
@@ -5,9 +5,16 @@
  
  For most apps you'll need to have a data structure to hold information about a user. Create a `User` struct that has properties for basic information about a user. At a minimum, it should have properties to represent a user's name, age, height, weight, and activity level. You could do this by having `name` be a `String`, `age` be an `Int`, `height` and `weight` be of type `Double`, and `activityLevel` be an `Int` that will represent a scoring 1-10 of how active they are. Implement this now.
  */
-
-
+struct User {
+    var name: String
+    var age: Int
+    var height: Double
+    var weight: Double
+    var activityLevel: Int
+}
 //:  Create a variable instance of `User` and call it your name. Use the memberwise initializer to pass in information about yourself. Then print out a description of your `User` instance using the instance's properties.
+let akshay = User(name: "Akshay", age: 19, height: 5.9, weight: 69.0, activityLevel: 7)
+print("Name: \(akshay.name), Age: \(akshay.age), Height: \(akshay.height) ft, Weight: \(akshay.weight) Kgs, Activity Level: \(akshay.activityLevel)")
 
 
 /*:
@@ -15,14 +22,34 @@
 
  - Example: If you use the initializer for meters and pass in a distance of 1600, the initializer should set `meters` to 1600 and `feet` to 5249.344.
  */
+struct Distance {
+    var meters: Double
+    var feet: Double
+    
+    init(meters: Double) {
+        self.meters = meters
+        self.feet = meters * 3.28084
+    }
+    
+    init(feet: Double) {
+        self.feet = feet
+        self.meters = feet / 3.28084
+    }
+}
+
+let distanceInMeters = Distance(meters: 1600)
+print("Distance in meters: \(distanceInMeters.meters), Distance in feet: \(distanceInMeters.feet)")
+
+let distanceInFeet = Distance(feet: 5249.344)
+print("Distance in feet: \(distanceInFeet.feet), Distance in meters: \(distanceInFeet.meters)")
 
 
 //:  Now create an instance of `Distance` called `mile`. Use the initializer for meters to set the distance to 1600. Print out the property for feet and verify that it is equal to 5249.344.
-
-
+let mile = Distance(meters: 1600)
+print("Distance in feet: \(mile.feet)")
 //:  Now create another instance of `Distance` and give it some other distance. Ensure that both properties are set correctly.
-
-
+let anotherDistance = Distance(meters: 800)
+print("Distance in meters: \(anotherDistance.meters), Distance in feet: \(anotherDistance.feet)")
 /*:
 [Previous](@previous)  |  page 4 of 10  |  [Next: Exercise - Methods](@next)
  */
